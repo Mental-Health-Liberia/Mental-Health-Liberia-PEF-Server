ca9e4c8ccc3aa477bb6604e1306079c595520aaa
Tanner Smith <tanner@tssoftware.net>
2013-10-23 10:06:16 -0400

    Initial commit (rails w/ mongo).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pef::Application.config.secret_token = '0af598e7f714b25d2af900bba6a47da257c2c458d7605bbc2b436afb82cb69b460feb720bf966969a8166730d6de7b338367a357521c30a20b7e0a1ab2044f3b'
