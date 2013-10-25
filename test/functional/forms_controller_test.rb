07cf85d49e19740e012db4a70430016ac9cdf016
Tanner Smith <tanner@tssoftware.net>
2013-10-25 11:04:20 -0400

    Write a test for create action for Forms Controller.

--- test/functional/forms_controller_test.rb
+++ test/functional/forms_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class FormsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create with no parameters" do
+    post(:create,  {})
+    assert_response 406, @response.body
+  end
+
+  test "should create with only format parameter" do
+    post(:create,  {:format => "json"})
+    assert_response :success, @response.body
+
+    body = @response.body
+    assert_nil body['format']
+    assert_nil body['controller']
+    assert_nil body['action']
+  end
 end
