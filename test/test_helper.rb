731332b766b0721d78d2ad4695341cff005c92d8
Tanner Smith <tanner@tssoftware.net>
2013-11-08 12:49:08 -0500

    Clean out the db before every test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,16 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Add more helper methods to be used by all tests here...
+  # Drop all collections after each test case.
+  def teardown
+    MongoConfig.db['forms'].remove()
+  end
+
+  # Make sure that each test case has a teardown
+  # method to clear the db after each test.
+  def inherited(base)
+    base.define_method :teardown do
+      super
+    end
+  end
 end
