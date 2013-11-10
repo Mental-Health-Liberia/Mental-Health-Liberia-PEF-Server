d46884d1c88581531f87000205af8d856379fd4c
Tanner Smith <tanner@tssoftware.net>
2013-11-10 16:22:49 -0500

    Add filter to not use CSRF for json requests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :assign_params
+
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < Devise::SessionsController
-  
   def create
     resource = warden.authenticate!(:scope => resource_name, :recall => :failure)
     return sign_in_and_redirect(resource_name, resource)
