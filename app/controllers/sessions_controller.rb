5b75603f7145ae830f644557f77e918ac2eb83d5
Ryan Ashcraft <ryanashcraft@gmail.com>
2013-11-11 08:18:21 -0500

    Fix issues with devise logging in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < Devise::SessionsController
+  skip_before_filter :require_no_authentication
+  before_filter :check_concurrent_session
+
+  def check_concurrent_session
+    if is_already_logged_in?
+      sign_out(current_user)
+    end
+  end
+
   def create
-    resource = warden.authenticate!(:scope => resource_name, :recall => :failure)
+    resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
     return sign_in_and_redirect(resource_name, resource)
   end
   
@@ -12,6 +21,12 @@ class SessionsController < Devise::SessionsController
   end
  
   def failure
-    return render:json => {:success => false, :errors => ["Login failed."]}
+    return render :json => {:success => false, :errors => ["The username or password you entered is incorrect."]}
+  end
+
+  protected 
+
+  def is_already_logged_in?
+    current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Pef::Application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: {:sessions => "sessions"}
 
   resources :forms
 
