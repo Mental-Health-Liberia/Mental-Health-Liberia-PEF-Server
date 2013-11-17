f271e849e2cf5d34777414370fb4d4c05fc04bb1
Tanner Smith <tanner@tssoftware.net>
2013-11-17 11:45:08 -0500

    Allow admins to edit and destroy users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,31 @@ class UsersController < ApplicationController
 
   before_filter :ensure_manage_users_access
 
+  def user_params
+    params["user"]
+  end
+
   def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 end
